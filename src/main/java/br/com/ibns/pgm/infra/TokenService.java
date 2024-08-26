814f6a450967983e7c115b6dcbb5f2c63b3bd0ed
Yoda43verMyRepos <yoda43vermyrepos@gmail.com>
2024-08-26 16:19:19 -0300

    udates TokenSerivice class

--- src/main/java/br/com/ibns/pgm/infra/TokenService.java
+++ src/main/java/br/com/ibns/pgm/infra/TokenService.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -16,11 +17,13 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    @Value("{api.security.token.secret}")
+    private String secret;
 
     //O metodo abaixo retorna uma String, que representa o token a ser gerado
     public String gerarToken(Usuario usuario) {
         try {
-            var algorithm = Algorithm.HMAC256("12345678");
+            var algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Cad_PGM")
                     .withSubject(usuario.getLogin())
